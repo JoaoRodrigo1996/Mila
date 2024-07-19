4be9ea2e5a92a8c396fa850d3b2634c81d92f082
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-19 12:22:41 -0300

    feat: create header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,41 @@
+import { Bell } from "lucide-react";
+
 export function App() {
   return (
-    <h1 className="text-3xl font-bold">Hello World</h1>    
+    <>
+      <header className="max-w-5xl mx-auto h-12 flex items-center justify-between">
+        <div className="flex items-center gap-5">
+          <img src="/logo.svg" alt="" className="" />
+          
+
+          <menu className="flex itemce gap-5">
+            <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Página inicial</a></li>
+            <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Histórico</a></li>
+          </menu>
+        </div>
+
+        <div className="flex items-center gap-4">
+          <div className="flex items-center justify-center gap-3">
+            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+              <Bell className="size-5" />
+            </button>
+          </div>
+
+          <div className="h-6 w-px bg-zinc-800" />
+
+          <button className="size-8 rounded-full">
+            <img 
+              src="https://github.com/JoaoRodrigo1996.png" 
+              alt="Foto de perfil" 
+              className="size-8 rounded-full object-cover" 
+            />
+          </button>
+        </div>
+      </header>
+
+      <main className="max-w-5xl mx-auto pt-5">
+        
+      </main>
+    </> 
   )
 }
