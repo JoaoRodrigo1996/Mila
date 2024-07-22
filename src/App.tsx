50d89c065e0c5049abc026b8c8abe31338f2bda2
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-22 12:37:54 -0300

    refactor: creating header component

--- src/App.tsx
+++ src/App.tsx
@@ -1,8 +1,9 @@
-import { ArrowDownRight, Bell, Ellipsis, Plus } from "lucide-react";
+import { ArrowDownRight, Ellipsis, Plus } from "lucide-react";
 import { faker } from '@faker-js/faker'
 import { format } from 'date-fns'
 import { ptBR } from 'date-fns/locale'
 import { LineChart, Line, Tooltip } from 'recharts'
+import { Header } from "./components/header";
 
 export function App() {
   const data = Array.from({ length: 10 }, () => {
@@ -18,33 +19,7 @@ export function App() {
 
   return (
     <div className='max-w-5xl mx-auto py-5 flex flex-col gap-5'>
-      <header className="h-12 flex items-center justify-between">
-        <div className="flex items-center gap-5">
-          <img src="/logo.svg" alt="" className="" />
-          <menu className="flex itemce gap-5">
-            <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Página inicial</a></li>
-            <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Histórico</a></li>
-          </menu>
-        </div>
-
-        <div className="flex items-center gap-4">
-          <div className="flex items-center justify-center gap-3">
-            <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
-              <Bell className="size-5" />
-            </button>
-          </div>
-
-          <div className="h-6 w-px bg-zinc-800" />
-
-          <button className="size-8 rounded-full">
-            <img 
-              src="https://github.com/JoaoRodrigo1996.png" 
-              alt="Foto de perfil" 
-              className="size-8 rounded-full object-cover" 
-            />
-          </button>
-        </div>
-      </header>
+      <Header />
 
       <main className="flex flex-col gap-4">
         <section className="flex items-center justify-between">
---
+++ src/components/header.tsx
@@ -0,0 +1,33 @@
+import { Bell } from "lucide-react";
+
+export function Header(){
+  return (
+    <header className="h-12 flex items-center justify-between">
+      <div className="flex items-center gap-5">
+        <img src="/logo.svg" alt="" className="" />
+        <menu className="flex itemce gap-5">
+          <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Página inicial</a></li>
+          <li><a href="#" className="text-sm text-zinc-400 focus:underline focus:underline-offset-4 focus:text-zinc-100 hover:text-zinc-100 hover:underline hover:underline-offset-4 transition-colors">Histórico</a></li>
+        </menu>
+      </div>
+
+      <div className="flex items-center gap-4">
+        <div className="flex items-center justify-center gap-3">
+          <button className='text-zinc-400 hover:text-zinc-100 transition-colors'>
+            <Bell className="size-5" />
+          </button>
+        </div>
+
+        <div className="h-6 w-px bg-zinc-800" />
+
+        <button className="size-8 rounded-full">
+          <img 
+            src="https://github.com/JoaoRodrigo1996.png" 
+            alt="Foto de perfil" 
+            className="size-8 rounded-full object-cover" 
+          />
+        </button>
+      </div>
+    </header>
+  )
+}
