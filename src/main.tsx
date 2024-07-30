79130282cc43a5286ed4d73c3b1095b1258f685c
Rodrigo Mesquita <dev.rodrigomesquita@gmail.com>
2024-07-30 16:47:31 -0300

    fix: fix file extentions

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,6 @@
 import React from 'react'
 import ReactDOM from 'react-dom/client'
-import { App } from './app.tsx'
+import { App } from './app'
 import { ClerkProvider } from '@clerk/clerk-react'
 import { dark } from '@clerk/themes'
 import { ptBR } from '@clerk/localizations'
